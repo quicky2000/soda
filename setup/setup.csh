ca6282e3fff98d014d86ab944119dc29d9f52db7
Julien Thevenon <julien_thevenon@yahoo.fr>
2008-08-19 01:37:58 +0200

    Update to mange both Linux and windows path splitting

--- setup/setup.csh
+++ setup/setup.csh
@@ -1,4 +1,5 @@
 #!/bin/tcsh
 setenv QUICKY_TOOLS ${PWD}/..
+setenv QUICKY_OS `uname -a | cut -d" " -f1`
 setenv PATH ${PATH}:${QUICKY_TOOLS}/bin
 #EOF
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 QUICKY_TOOLS=${PWD}/..
 export QUICKY_TOOLS
+QUICKY_OS=`uname -a | cut -d" " -f1`
+export QUICKY_OS
 PATH=${PATH}:${QUICKY_TOOLS}/bin
 export PATH
 #EOF
